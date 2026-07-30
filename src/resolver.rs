a591c73f89b2947e93ff287f9ae85e8e9d946004
Alex Puschev <puschev.alex@gmail.com>
2026-07-30 12:46:42 +0300

    chore: update wrangler version and allow specific scripts in package.json
    feat: modify PendingItem initialization in resolver.rs to include an additional parameter

--- src/resolver.rs
+++ src/resolver.rs
@@ -43,7 +43,7 @@ impl Resolver {
             .map(|arr| arr.iter().filter_map(|t| t.as_str().map(|s| s.to_string())).collect())
             .unwrap_or_default();
 
-        let mut item = PendingItem::new(name.to_string(), KnowledgeType::Link);
+        let mut item = PendingItem::new(name.to_string(), KnowledgeType::Link, 0);
         item.provider = ResourceProvider::Github;
         item.description = description.map(|s| s.to_string());
         item.language = language.map(|s| s.to_string());
